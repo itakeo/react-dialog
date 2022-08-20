522ac8d4868c615144a83ae9e691545d578229e0
Cheng <354962250@qq.com>
2022-08-20 15:53:00 +0800

    Update index.jsx

--- src/utils/dialog/index.jsx
+++ src/utils/dialog/index.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './dialog.css'
 let __dialog_index_num = 0;
 class DialogCom extends React.Component {
+    constructor(props) {
+        super(props);
+    }
     state = {
         content : '',
         time : null,
@@ -17,25 +20,34 @@ class DialogCom extends React.Component {
         onLoad : ()=>null,
         onClose : ()=>null
     }
-
     componentDidMount(){
         let _button = this.props.button,_buttonArr = [];
-        _button && Object.keys(_button).forEach(key=>{
-            _buttonArr.push({
-                title : key,
-                fn : _button[key]
+        if(_button && (_button instanceof Array)){
+            _button.map((o,i)=>{
+                _buttonArr.push({
+                    title : o.title,
+                    fn : o.onClick || o.onclick
+                });
             });
-        });
+        }else{
+            _button && Object.keys(_button).forEach(key=>{
+                _buttonArr.push({
+                    title : key,
+                    fn : _button[key]
+                });
+            });
+        }
         this.setState({
             buttonArr : _buttonArr,
-            content : this.props.content,
             ...this.props
         },()=>{
-            this.state.onLoad(this);
-            if(this.state.time){
-                setTimeout(()=>{
-                    this.close(this.state.index,this,this.state.onClose,this.state.root);
-                },this.state.time)
+            if(this.props.visible === undefined){//判断是否是指令式调用
+                this.state.onLoad(this);
+                if(this.state.time){
+                    setTimeout(()=>{
+                        this.close(this.state.index,this,this.state.onClose,this.state.root);
+                    },this.state.time)
+                }
             }
         });
     }
@@ -45,7 +57,8 @@ class DialogCom extends React.Component {
         removeDialogElement(a,b,c,d);
     }
     render(){
-        let {content,mask,title,buttonArr,ui,maskClick} = this.state
+        let {buttonArr} = this.state
+        let {content,mask=true,title,ui,maskClick=true} = this.props
         return (
             <>
                 {
@@ -54,6 +67,7 @@ class DialogCom extends React.Component {
                         {title ? <div className="c_alert_title">{title}</div> : ''}
                         <div className="c_alert_con">
                             {content}
+                            {this.props.children && this.props.children}
                         </div>
                         {
                             buttonArr.length ? (
@@ -119,10 +133,11 @@ function createDialogElement(a,b){
         _dialogElement.classList.add('c_alert_inner');
     }
     _props.addClass && _dialogElement.classList.add(_props.addClass);
-    _dialogElement.style.zIndex = _props.zIndex;
     let _index = _props.index ? _props.index : __dialog_index_num;
     _dialogElement.dataset.index = _index;
     document.body.appendChild(_dialogElement);
+    //判断是否是指令式调用
+    if(_dialogElement.parentNode !== document.body && !_props.visible) return
     let root = ReactDOM.createRoot(_dialogElement);
     root.render( <DialogCom content={_content} time={_time} root={root} index={_index} {..._props} />);
     return {close : (fn)=>{
@@ -130,6 +145,31 @@ function createDialogElement(a,b){
     }}
 }
 
-export default function Dialog(a,b){
-   return createDialogElement(a,b)
+
+class StatementDialog extends React.Component {
+    constructor(props) {
+        super(props);
+        this.target = React.createRef();
+    }
+    render(){
+        if(this.target.current){
+            if(!this.props.visible){
+                this.props.onclose && this.props.onclose();
+                this.props.onClose && this.props.onClose();
+            } else{
+                this.props.onload && this.props.onload()
+                this.props.onLoad && this.props.onLoad()
+            };
+        };
+        return <div ref={this.target} className={"c_alert_dialog  " + (this.props.addClass)+ (this.props.visible ? ' dialog_open ' :' ') + (!(this.props.ui || this.props.title || this.props.button) ? 'c_alert_inner' : '')}>
+            <DialogCom {...this.props} />
+        </div>
+    }
+
+}
+
+export default function Dialog(props,b){
+    if(!(props.visible === undefined)){
+        return <StatementDialog {...props}/>
+    }else  return createDialogElement(props,b)
 }
